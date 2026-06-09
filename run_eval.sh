d4e50f49a06a85ceb74bc341828aef846fcd7cb2
yohei-kobashi <yohei.kobashi@gmail.com>
2026-06-10 04:26:37 +0900

    misc

--- run_eval.sh
+++ run_eval.sh
@@ -7,10 +7,15 @@
 module load singularity
 cd CodeGeeX
 source env/bin/activate
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5.csv
 bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5_grpo_reward7b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5_grpo_reward7b.csv
 bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5_grpo_reward30b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5_grpo_reward30b.csv
 bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5_grpo_reward80b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5_grpo_reward80b.csv
-bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reword7b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward7b.csv
-bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reword30b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward30b.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reward7b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward7b.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reward30b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward30b.csv
 bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reward80b codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward80b.csv
-bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5_grpo_reward30b_v2_100 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5_grpo_reward30b_v2_100.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen2.5_grpo_reward30b_v2_194 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen2.5_grpo_reward30b_v2_194.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reward80b_v2_100 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward80b_v2_100.csv
+bash scripts/evaluate_humanevalx_all.sh evaluation_qwen3.5_grpo_reward80b_v2_194 codegeex/benchmark/humaneval-x/humanevalx.sif 64 30 evaluation_qwen3.5_grpo_reward80b_v2_194.csv
